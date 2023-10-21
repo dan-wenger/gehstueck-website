8ffc01f83fa0b4f35b5e68385fdf8636e6d93281
dan-wenger <18039819+dan-wenger@users.noreply.github.com>
2023-10-21 22:37:13 +0200

    Updates

--- _app/immutable/chunks/paths.e9cdbac5.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_4b3bx0)==null?void 0:s.base)??"/gehstueck-website";var e;const b=((e=globalThis.__sveltekit_4b3bx0)==null?void 0:e.assets)??t;export{b as a,t as b};
--- _app/immutable/nodes/2.edded858.js
+++ _app/immutable/nodes/2.edded858.js
@@ -1,26 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<link rel="icon" href="./favicon.png" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		
-		<link href="./_app/immutable/assets/2.e43d5882.css" rel="stylesheet">
-		<link rel="modulepreload" href="./_app/immutable/entry/start.9f384351.js">
-		<link rel="modulepreload" href="./_app/immutable/chunks/scheduler.cc1c0861.js">
-		<link rel="modulepreload" href="./_app/immutable/chunks/singletons.f0eb33d9.js">
-		<link rel="modulepreload" href="./_app/immutable/entry/app.3a47fc2b.js">
-		<link rel="modulepreload" href="./_app/immutable/chunks/index.7f2fc51f.js">
-		<link rel="modulepreload" href="./_app/immutable/nodes/0.90cc561c.js">
-		<link rel="modulepreload" href="./_app/immutable/nodes/2.edded858.js">
-	</head>
-	<body data-sveltekit-preload-data="hover">
-		<div style="display: contents">  <main class="svelte-1e4um7i"><div style="display: inline-block; width: 100%; height: 100%;"><h1 class="svelte-1e4um7i" data-svelte-h="svelte-1hcylf7">Gehstück, das.</h1></div> <p id="subline" class="svelte-1e4um7i" data-svelte-h="svelte-xv24kc">Ein Text, für den <span class="wider svelte-1e4um7i">Spaziergang</span> geschrieben,
+import{s as L,f as A,n as b}from"../chunks/scheduler.cc1c0861.js";import{S as I,i as y,g as m,s as z,h as g,j as D,y as o,f as w,c as G,k as d,a as H,x as u}from"../chunks/index.7f2fc51f.js";const C=(s,{min_size:e=10,max_size:t=100}={min_size:10,max_size:100})=>{const n=()=>{s.parentElement&&P(s,s.parentElement,e,t)},a=new ResizeObserver(n);return a.observe(s==null?void 0:s.parentElement),{destroy(){a.disconnect()}}};function N({clientWidth:s,clientHeight:e,scrollWidth:t,scrollHeight:n}){return t>s||n>e}function P(s,e,t,n){let a=t,h=!1,i=t;for(s.style.fontSize=`${i}px`;!h&&a<n;)h=N(e),h||(s.style.fontSize=`${a}px`,a++);i=a-2,s.style.fontSize=`${i}px`}const R="display: inline-block; width: 100%; height: 100%;";function W(s){let e,t,n,a="Gehstück, das.",h,i,T=`Ein Text, für den <span class="wider svelte-1e4um7i">Spaziergang</span> geschrieben,
     gelangt durch den <span class="wider svelte-1e4um7i">Spaziergang</span> zur Geltung. Wenn
     Du noch nie davon gehört hast, dann deshalb, weil ich das Gehstück als
     <span class="wider svelte-1e4um7i">Textgattung</span>
     erst begründe, indem Du das liest, gerade
-    <span class="wider svelte-1e4um7i">begründet</span> habe.</p> <div id="paragraphen" class="svelte-1e4um7i" data-svelte-h="svelte-1t9dpsq"><p class="svelte-1e4um7i">§1 Das Gehstück bedient sich des Gehens als kulturelle Technik der
+    <span class="wider svelte-1e4um7i">begründet</span> habe.`,v,r,x=`<p class="svelte-1e4um7i">§1 Das Gehstück bedient sich des Gehens als kulturelle Technik der
       Wahrnehmung und des Denkens.</p> <p class="svelte-1e4um7i">§2 Das Gehstück bietet eine besondere Art von Texterfahrung durch den
       Einbezug ALLER SINNE.</p> <p class="svelte-1e4um7i">§3 Das Gehstück ist ZUGÄNGLICH, wie es Kunst und Literatur gerne von sich
       behaupten, es aber nur selten sind. Im Gehen finden wir einen
@@ -48,37 +31,8 @@
       eines Spaziergangs.</p> <p class="svelte-1e4um7i">§11 Das Gehstück überzeugt AUS SICH. Alle Paragrafen hier sind
       intellektueller Anhang oder auch – im Fall dieser Einladungskarte –
       Zierde. Du musst die aufgezählten Punkte weder gelesen noch verstanden
-      haben, um an einem Gehstück teilzunehmen.</p></div> <div class="container-right svelte-1e4um7i" data-svelte-h="svelte-ew24vl"><p class="svelte-1e4um7i">Basel, Januar 2023 Léonard Wiesendanger</p> <p class="svelte-1e4um7i">Geschrieben zum Auftakt der Lesungsreihe „Gehstück, das.“, in welcher
+      haben, um an einem Gehstück teilzunehmen.</p>`,f,c,S=`<p class="svelte-1e4um7i">Basel, Januar 2023 Léonard Wiesendanger</p> <p class="svelte-1e4um7i">Geschrieben zum Auftakt der Lesungsreihe „Gehstück, das.“, in welcher
       gattungsfremde Texte spazierfertig gemacht wurden mit dem Ziel, die
       Eigenständigkeit der neuen Textgattung „Gehstück“ besser verstehen und
       begründen zu können.</p> <h2 class="svelte-1e4um7i">OPEN CALL</h2> <p class="svelte-1e4um7i">Einsendungen für die Lesungsreihe „Gehstück, das. 2024“ an
-      vvvw[at]bluewin.ch</p></div> </main> 
-			
-			<script>
-				{
-					__sveltekit_1sofygp = {
-						assets: "/gehstueck-website",
-						base: new URL(".", location).pathname.slice(0, -1),
-						env: {}
-					};
-
-					const element = document.currentScript.parentElement;
-
-					const data = [null,null];
-
-					Promise.all([
-						import("./_app/immutable/entry/start.9f384351.js"),
-						import("./_app/immutable/entry/app.3a47fc2b.js")
-					]).then(([kit, app]) => {
-						kit.start(app, element, {
-							node_ids: [0, 2],
-							data,
-							form: null,
-							error: null
-						});
-					});
-				}
-			</script>
-		</div>
-	</body>
-</html>
+      vvvw[at]bluewin.ch</p>`,k,_;return{c(){e=m("main"),t=m("div"),n=m("h1"),n.textContent=a,h=z(),i=m("p"),i.innerHTML=T,v=z(),r=m("div"),r.innerHTML=x,f=z(),c=m("div"),c.innerHTML=S,this.h()},l(p){e=g(p,"MAIN",{class:!0});var l=D(e);t=g(l,"DIV",{style:!0});var E=D(t);n=g(E,"H1",{class:!0,"data-svelte-h":!0}),o(n)!=="svelte-1hcylf7"&&(n.textContent=a),E.forEach(w),h=G(l),i=g(l,"P",{id:!0,class:!0,"data-svelte-h":!0}),o(i)!=="svelte-xv24kc"&&(i.innerHTML=T),v=G(l),r=g(l,"DIV",{id:!0,class:!0,"data-svelte-h":!0}),o(r)!=="svelte-1t9dpsq"&&(r.innerHTML=x),f=G(l),c=g(l,"DIV",{class:!0,"data-svelte-h":!0}),o(c)!=="svelte-ew24vl"&&(c.innerHTML=S),l.forEach(w),this.h()},h(){d(n,"class","svelte-1e4um7i"),d(t,"style",R),d(i,"id","subline"),d(i,"class","svelte-1e4um7i"),d(r,"id","paragraphen"),d(r,"class","svelte-1e4um7i"),d(c,"class","container-right svelte-1e4um7i"),d(e,"class","svelte-1e4um7i")},m(p,l){H(p,e,l),u(e,t),u(t,n),u(e,h),u(e,i),u(e,v),u(e,r),u(e,f),u(e,c),k||(_=A(C.call(null,n)),k=!0)},p:b,i:b,o:b,d(p){p&&w(e),k=!1,_()}}}class K extends I{constructor(e){super(),y(this,e,null,W,L,{})}}export{K as component};
