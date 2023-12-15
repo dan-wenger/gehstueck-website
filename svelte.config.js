2f053ffc2a1862f0b3f316d8d78c194d45d61e32
dan-wenger <18039819+dan-wenger@users.noreply.github.com>
2023-12-15 23:39:21 +0100

    changed back to adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,7 @@ import adapter from "@sveltejs/adapter-auto";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapterStatic(),
     paths: {
       base: process.env.NODE_ENV === "production" ? "/gehstueck-website" : "",
     },
