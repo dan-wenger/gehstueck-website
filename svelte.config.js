c3537da985800e3665e45ad6ed0af8dd5bd6b65d
dan-wenger <18039819+dan-wenger@users.noreply.github.com>
2024-01-09 13:18:51 +0100

    back to adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      runtime: 'nodejs18.x'
+    }),
     paths: {
       base: process.env.NODE_ENV === "production" ? "/gehstueck-website" : "",
     },
