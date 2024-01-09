c7b583a4bc55f334433e318ace9d37945c3620a1
dan-wenger <18039819+dan-wenger@users.noreply.github.com>
2024-01-09 13:21:46 +0100

    this is it maybe.. deleted paths config

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,8 @@ import adapter from "@sveltejs/adapter-vercel";
 const config = {
   kit: {
     adapter: adapter({
-      runtime: 'nodejs18.x'
+      runtime: "nodejs18.x",
     }),
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "/gehstueck-website" : "",
-    },
   },
 };
 
