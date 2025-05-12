ca65ca7ac84c9973f7ff8cc988d2d816a99fec56
Xlozy <110845619+Xlozy@users.noreply.github.com>
2025-05-12 20:52:58 +0200

    inhaltliche anpassungen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 <main lang="de">
   <h1 class="titel">Gehstück, das.</h1>
   <p class="subline blocksatz" lang="de">
-    Ein Text für das Gehen geschrieben, gelangt durch das Gehen zur Geltung. Wenn Du noch nie davon gehört hast, dann deshalb, weil ich das Gehstück als Textgattung erst begründe, indem Du das liest, gerade begründet habe.
+    Ein Text fürs Gehen geschrieben, gelangt im Gehen zur Geltung. Wenn Du noch nie davon gehört hast, dann deshalb, weil wir das Gehstück als Textgattung erst begründen, indem Du das liest, gerade begründet haben.
   </p>
   <div class="blocksatz paragraphen">
     <p>
@@ -79,7 +79,7 @@
     </p>
   </div>
   <div class="container-right">
-    <p>Léonard Wiesendanger<br />Basel, Januar 2023</p>
+    <p>LJVW<br/>Basel, Januar 2023</p>
     <p>
       Geschrieben zum Auftakt der Lesungsreihe „Gehstück, das.“ (2023), in
       welcher gattungsfremde Texte spazierfertig gemacht wurden mit dem Ziel,
@@ -87,99 +87,7 @@
       begründen zu können.
     </p>
   </div>
-  <div class="abstandhalter"></div>
-
-  <div class="blocksatz paragraphen">
-    <p>
-      §12 Ein Gehstück kann als LESUNG auf einem Spaziergang in Szene gesetzt
-      werden. Die Lesung ist die niederschwelligste Art, ein Gehstück zu
-      erfahren.
-    </p>
-    <p>
-      §13 Ein Gehstück funktioniert in der selbständigen LEKTÜRE. Dafür wurde
-      ein Format entwickelt, das dem Lesen unterwegs entspricht. Die sich daraus
-      ergebende Geh-Lese-Praxis ist persönlich und fällt von Person zu Person
-      verschieden aus.
-    </p>
-    <p>
-      §14 Ein spontanes Gehstück kann einer gemeinschaftlichen SCHREIBÜBUNG
-      entgehen. Dafür verfassen alle Teilnehmer:innen vor Ort oder im Voraus
-      einen Text, um sich diesen dann beim gemeinsamen Spaziergang vorzulesen.
-      Eine Person geht voraus, alle setzen mit ihrem Text ein, wann und wo sie
-      es für richtig halten.
-    </p>
-    <p>
-      §15 Die GEH-HANDLUNG ist das kennzeichnende Merkmal des Gehstücks. Sie
-      geht in Echtzeit aus dem Zusammenwirken von Gehen und Texthandlung hervor.
-      Alle weiteren Merkmale des Gehstücks sind der Geh-Handlung untergeordnete:
-      indem sie das Zusammenwirken von Gehen und Texthandlung befördern.
-    </p>
-    <p>
-      §16 Jeder Text führt eine Handlung im weitesten Sinn. ALLES kann auf und
-      entlang eines Weges erzählt werden.
-    </p>
-    <p>
-      §17 Das Gehstück braucht PRÄGNANZ, um auf einem einzigen Spaziergang
-      gelesen werden zu können und sich in den Raum einzuschreiben.
-    </p>
-    <p>§18 Das Gehstück kann REGIEANWEISUNGEN haben, die das Gehen anleiten.</p>
-    <p>
-      §19 Das Gehstück funktioniert (wie Sprache immer) DE-/REKONTEXTUALISIERT.
-      Selbst wenn ein Gehstück für einen bestimmten Ort geschrieben wurde, kann
-      es auch an einem anderen Ort gelesen werden. Seine Lektüre ist an keinen
-      bestimmten Ort gebunden.
-    </p>
-    <p>
-      §20 Kein Text wird zweimal auf dieselbe Weise gelesen. Was sich bei
-      herkömmlichen Texten verschiebt, beginnt im bewegten Kontext des Gehstücks
-      zu springen: Die Bedeutung des Textes wird im Feld unberechenbar. Ein
-      Gehstück zeichnet sich aus durch besondere WIEDERHOLBARKEIT. Es will
-      mehrmals, auf verschiedenen Spaziergängen gelesen werden.
-    </p>
-    <p>
-      §21 Kunst ist der RAHMEN, nicht das Bild. All die Ausstellungs- und
-      Bühnenräume und Museen mit ihren Anbetungskästen, Bühnen und Podesten
-      haben es verkehrt. Das Leben ist das Bild und die Kunst der vielgestaltige
-      Rahmen, der uns ermöglicht, es mit neuen Augen zu sehen. Das Gehstück
-      nimmt diesen Rahmen und hält ihn wieder dorthin, wo er hingehört. Wer ein
-      Gehstück mitgeht, erlebt was vergleichbar ist mit der Schöpfung eines
-      Kunstwerks: ein persönlicher Zusammenhang erschliesst sich.
-    </p>
-    <p>
-      §22 Das Gehstück UNTERLÄUFT die etablierten Literaturformate und ihre
-      Institutionen. Das Gehstück bedarf keiner Bühne, keiner Infrastruktur und
-      ist deshalb keinen Machtasymmetrien unterworfen. Einfach Literatur
-      miteinander gehen.
-    </p>
-    <p>
-      §23 Das Gehstück ist das SCHRITTMASS der Literatur, der Anspruch ist
-      holistisch. Schritt für Schritt wird angegangen: Der Text, das Lesen, das
-      Schreiben.
-    </p>
-    <p>
-      §24 Ziel des Gehstücks ist die ERNEUERUNG der Beziehung des Menschen zu
-      seiner Umwelt. Weder braucht es dafür technisch aufwändig hergestellte
-      Immersion noch Flugzeug-Ferien auf einem versinkenden Inselstaat.
-    </p>
-    <p>
-      §25 Das Gehstück unterhält SPIELERISCH. Es bildet in dem Masse wie der
-      Blick auf die Umwelt geprägt wird, mühelos. Was davon haften bleibt, was
-      davon vergeht… wer spaziert oder was sich beim Spazieren präsentiert,
-      nichts drängt sich auf.
-    </p>
-    <p>
-      §26 Das Gehstück ist bloss ein weiterer VERSUCH in einer jahrtausendealten
-      Versuchsgeschichte, in der immer schon um adäquate, zeitgemässe,
-      problemgerechte und letztlich persönliche Ausdrucksformen und Sprachen
-      gerungen wurde. Wer an dieser Gehbewegung Gefallen findet, geht sie mit,
-      geht sie weiter und seinen oder ihren eigenen Weg.
-    </p>
-  </div>
-
-  <div class="container-right">
-    <p>Léonard Wiesendanger<br />Basel, Februar 2024</p>
-    <p>Geschrieben zum Auftakt der Lesungsreihe „Gehstück, das. 24/1“.</p>
-  </div>
+  
   <div class="abstandhalter"></div>
 
   <div class="container-right">
@@ -196,8 +104,8 @@
     <p>
       Das Gehstück ist eine öffnende Bewegung: Wer sich daran versuchen
       will, Texte und Ideen an
-      <a href="mailto:info@gehstueck.ch">info[at]gehstueck.ch</a>. Gerne gebe
-      ich Rückmeldung, auch im Hinblick auf die anstehende Programmplanung.
+      <a href="mailto:info@gehstueck.ch">info[at]gehstueck.ch</a>. Gerne geben
+      wir Rückmeldung, auch im Hinblick auf die anstehende Programmplanung.
     </p>
   </div>
 
